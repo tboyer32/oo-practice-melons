7dc9815c758a073cdcac23aaf490dcc5e031951d
Tiffany Boyer <tboyercode@gmail.com>
2022-03-24 15:03:05 -0500

    started part 2

--- harvest.py
+++ harvest.py
@@ -90,15 +90,66 @@ class Melon:
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
-    def __init__(self, is_sellable)
+    def __init__(self, melon_type, shape_rating, color_rating, harvested_from, harvested_by):
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.harvested_from = harvested_from
+        self.harvested_by = harvested_by
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
+    
 
-    # Fill in the rest
+    # Melon type: yw
 
+    # Shape rating: 8
+
+    # Color rating: 7
+
+    # Harvested from Field 2
+
+    # Harvested by Sheila
+
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+    melons = []
+
+    melon_1 = Melon(melons_by_id['yw'], 8, 7, 2, 'Sheila')
+    melon_2 = Melon(melons_by_id['yw'], 3, 4, 2, 'Sheila')
+    melon_3 = Melon(melons_by_id['yw'], 9, 8, 3, 'Sheila')
+    melon_4 = Melon(melons_by_id['cas'], 10, 6, 35, 'Sheila')
+    melon_5 = Melon(melons_by_id['cren'], 8, 9, 35, 'Michael')
+    melon_6 = Melon(melons_by_id['cren'], 8, 2, 35, 'Michael')
+    melon_7 = Melon(melons_by_id['cren'], 2, 3, 4, 'Michael')
+    melon_8 = Melon(melons_by_id['musk'], 6, 7, 4, 'Michael')
+    melon_9 = Melon(melons_by_id['yw'], 7, 10, 3, 'Sheila')
+
+    melons.extend(
+        [
+            melon_1,
+            melon_2,
+            melon_3,
+            melon_4,
+            melon_5,
+            melon_6,
+            melon_7,
+            melon_8,
+            melon_9
+        ]
+    )
+    return melons
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
+    sellability = None
 
-    # Fill in the rest
+    for melon in melons:
+        if melon.shape_rating > 5 and melon.color_rating > 5 and melon.harvested_from not 3:
+            sellability = "CAN BE SOLD"
+        else: 
+            sellability = "NOT SELLABLE"
+
+        print(f"Harvested by: {melon.harvested_by} from Field {melon.harvested_from} ({sellability})")
+
+    
